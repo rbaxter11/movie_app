b7074764c9d68001bcfa64d38456f9856f5b6348
ryan <ryanandrewbaxter@gmail.com>
2020-11-05 09:31:42 -0600

    Adds director and english, incomplete with error

--- app/controllers/api/movies_controller.rb
+++ app/controllers/api/movies_controller.rb
@@ -1,41 +1,45 @@
 class Api::MoviesController < ApplicationController
   def show
     input = params["id"]
-    @movie = Movie.find_by(id: input)
+    @movies = Movie.find_by(id: input)
     render 'show.json.jb'
   end
 
   def index
-    @movie = Movie.all 
+    @movies = Movie.all 
     render 'index.json.jb'
   end
 
   def create
-    @movie = Movie.new ({
+    @movies = Movie.new ({
       title: params["title"],
       year: params["year"],
       plot: params["plot"],
+      director: params["director"],
+      english: params["english"],
     })
-    @movie.save
+    @movies.save
     render 'show.json.jb'
   end
 
   def update
     input = params["id"]
-    @movie = Movie.find_by(id: input)
+    @movies = Movie.find_by(id: input)
 
-    @movie.title = params["title"] || @movie.title
-    @movie.year = params["year"] || @movie.year
-    @movie.plot = params["plot"] || @movie.plot
+    @movies.title = params["title"] || @movie.title
+    @movies.year = params["year"] || @movie.year
+    @movies.plot = params["plot"] || @movie.plot
+    @movies.director = params["director"] || @movie.director
+    @movies.english = params["english"] || @movie.english
     
-    @movie.save
+    @movies.save
     render 'show.json.jb'
   end
 
   def destroy
     input = params["id"]
-    @movie = Movie.find_by(id: input)
-    @movie.destroy
+    @movies = Movie.find_by(id: input)
+    @movies.destroy
     render json: {message: "Movie removed!"}
   end
 end
